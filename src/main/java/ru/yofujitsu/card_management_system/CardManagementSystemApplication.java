d83358a31d74b03c111a1802f2208368f72a61b3
yofujitsu <yofujitsuuu@gmail.com>
2025-09-16 18:56:40 +0300

    master: init commit

---
+++ src/main/java/ru/yofujitsu/card_management_system/CardManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.yofujitsu.card_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yofujitsu/card_management_system/CardManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yofujitsu.card_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
